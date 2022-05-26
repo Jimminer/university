dfd68d4a5293114d31da936c3af72c99db5a25a5
Jimminer <jimminer2003@gmail.com>
2022-05-26 17:26:18 +0300

    Fixed something in the 3rd exercise

--- examino2/operating systems/ergasia3/3.sh
+++ examino2/operating systems/ergasia3/3.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-if [ $(find . -maxdepth 1 -type d -name "$1" | wc -l) -eq 0 ]; then
-	mkdir "Script 3 files"
+output="Script 3 files"
+
+if [ $(find . -maxdepth 1 -type d -name "$output" | wc -l) -eq 0 ]; then
+	mkdir "$output"
 fi
 
-cd "Script 3 files"
+cd "$output"
 
 for i in {1..150}; do
 	echo "$i" > "file$i"
